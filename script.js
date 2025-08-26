f8b7b970855a9c600d27b1f75e6eabd1ce3aabcd
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-08-26 15:32:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+const form = document.querySelector("form");
+
+form.addEventListener('submit', (e)=>{
+	e.preventDefault();
+
+	const name = document.getElementById("name").value.trim();
+	const year = document.getElementById("year").value.trim();
+	const resultElement = document.getElementById("url");
+
+	let queryParam =[];
+
+	if(name) queryParam.push(`name=${encodeURIComponent(name)}`);
+	if(year) queryParam.push(`year=${encodeURIComponent(year)}`);
+
+	const finalResult =  `https://localhost:8080/?`${queryParam.join("&")`};
+	resultElement.innerText=finalResult;
+})
